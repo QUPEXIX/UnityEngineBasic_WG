b08305d4ea1fffea26517debd92301631a6d09fc
QUPEXIX <agi179@naver.com>
2023-09-16 18:49:08 +0900

    C# - 연산자, 배열, 반복문
    1. 연산자
    2. 배열
    3. 반복문

---
+++ CSharp/Array/Program.cs
@@ -0,0 +1,196 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        // 2차원 배열
+        static int[,] map = new int[6, 5] // 아이템이 6개 있고 각 아이템은 5개의 서브 아이템을 가짐. or 6개의 행과 5개의 열이 있음(메모리 상에서는 표 형식이 아니고 6개의 아이템이 일렬로 붙어 있음).
+        {
+                {0,0,0,0,1},
+                {0,1,1,1,1},
+                {0,0,0,1,1},
+                {1,1,0,1,1},
+                {1,1,0,1,1},
+                {1,1,0,0,2} // 0: 길 / 1: 벽 / 2: 도착 지점 / 3: 플레이어
+        };
+
+        static int y, x;
+
+        static void Main(string[] args)
+        {
+            #region 1차원 배열 및 반복문
+            int[] arr = new int[5]; // 배열은 참조 타입임.
+            arr[0] = 1; // []를 인덱서(인덱스에 접근하기 위한 연산자), 0을 인덱스(인덱서에서 몇 번째 인덱스에 접근할 것인지에 대한 입력)라고 함.
+            arr[1] = 3;
+
+            // 배열의 인덱스 접근: "배열 참조 주소 + 인덱스 * 자료형 크기"부터 자료형 크기(여기서는 int이므로 4byte)만큼 접근
+            // 배열을 만들 때 초기값을 명시하지 않으면 전부 defalt 값으로 초기화됨.
+            // 배열도 멤버 변수, 함수 등을 가지는 클래스 형태임.
+            Console.WriteLine(arr[0]);
+            Console.WriteLine(arr[1]);
+            Console.WriteLine(arr[2]);
+            Console.WriteLine(arr[3]);
+            Console.WriteLine(arr[4]); // 배열의 인덱스 범위는 0부터 Array.Length - 1
+
+            // while문: 조건이 참일 경우 반복 수행
+            int index = 0;
+            while (index < arr.Length)
+            {
+                Console.WriteLine(arr[index++]); // 후위 연산자를 활용하여, 대입한 후 index가 1만큼 늘어나게 함.
+            }
+
+            // do while문: 일단 한 번 실행한 후 조건이 참일 경우 반복 수행
+            //do
+            //{
+            //   반복할 내용
+            //} while (반복할 조건);
+
+            // for문: 
+            //for (반복 시작 전 실행할 내용; 반복할 조건; 반복 수행 완료 시마다 실행할 내용)
+            //{
+            //   반복할 내용
+            //}
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            // 배열 복사
+            int[] arr2 = new int[8];
+            System.Array.Copy(arr, arr2, arr.Length);
+
+            // 배열 복사를 for문으로 구현
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr2[i] = arr[i];
+            }
+            #endregion
+
+            map[y, x] = 3;
+            int goaly = 5;
+            int goalx = 4;
+            DrawMap();
+            while (y != goaly || x != goalx)
+            {
+                Console.WriteLine("w, a, s, d 중 하나를 입력하세요.");
+                string input = Console.ReadLine(); // 입력이 들어올때까지 기다리다가 입력값을 문자열로 반환
+                input = input.ToUpper(); // 문자열을 대문자로 변환
+                if (input == "W") MoveUp();
+                else if (input == "S") MoveDown();
+                else if (input == "D") MoveRight();
+                else if (input == "A") MoveLeft();
+                else Console.WriteLine("잘못된 입력입니다. w, a, s, d중 하나를 입력하세요.");
+            }
+            Console.WriteLine("보물찾기 끝! 게임 종료.");
+        }
+
+        static void DrawMap()
+        {
+            Console.WriteLine("--------------------");
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == 0)
+                        Console.Write("□");
+                    else if (map[i, j] == 1)
+                        Console.Write("■");
+                    else if (map[i, j] == 2)
+                        Console.Write("☆");
+                    else if (map[i, j] == 3)
+                        Console.Write("▣");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------");
+        }
+
+        static void MoveRight()
+        {
+            // 맵 범위 초과 확인
+            if (x >= map.GetLength(1) - 1)
+            {
+                Console.WriteLine("맵 바깥으로 나갈 수는 없다.");
+                return;
+            }
+
+            // 벽 확인
+            if (map[y, x + 1] == 1)
+            {
+                Console.WriteLine("벽으로 막혀있다.");
+                return;
+            }
+
+            map[y, x] = 0;
+            x++;
+            map[y, x] = 3;
+            DrawMap();
+        }
+
+        static void MoveLeft()
+        {
+            // 맵 범위 초과 확인
+            if (x <= 0)
+            {
+                Console.WriteLine("맵 바깥으로 나갈 수는 없다.");
+                return;
+            }
+
+            // 벽 확인
+            if (map[y, x - 1] == 1)
+            {
+                Console.WriteLine("벽으로 막혀있다.");
+                return;
+            }
+
+            map[y, x] = 0;
+            x--;
+            map[y, x] = 3;
+            DrawMap();
+        }
+
+        static void MoveUp()
+        {
+            // 맵 범위 초과 확인
+            if (y <= 0)
+            {
+                Console.WriteLine("맵 바깥으로 나갈 수는 없다.");
+                return;
+            }
+
+            // 벽 확인
+            if (map[y - 1, x] == 1)
+            {
+                Console.WriteLine("벽으로 막혀있다.");
+                return;
+            }
+
+            map[y, x] = 0;
+            y--;
+            map[y, x] = 3;
+            DrawMap();
+        }
+
+        static void MoveDown()
+        {
+            // 맵 범위 초과 확인
+            if (y >= map.GetLength(0) - 1)
+            {
+                Console.WriteLine("맵 바깥으로 나갈 수는 없다.");
+                return;
+            }
+
+            // 벽 확인
+            if (map[y + 1, x] == 1)
+            {
+                Console.WriteLine("벽으로 막혀있다.");
+                return;
+            }
+
+            map[y, x] = 0;
+            y++;
+            map[y, x] = 3;
+            DrawMap();
+        }
+    }
+}
---
+++ CSharp/Operators/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Operators // Operators와 같은 것을 식별문자라고 함. 게임 id와 같은 개념이며, using Operators;와 같은 형식으로 namespace를 갖다 쓸 수 있음.
+{
+    internal class Program // Operator라는 식별문자를 통해서만 Program에 접근이 가능함.
+    {
+        static void Main(string[] args)
+        {
+            int a = 14;
+            int b = 6;
+            int c = 0;
+
+            // 산술 연산자
+            c = a + b; Console.WriteLine(c); // a + b 연산 후 c 메모리(변수)에다 결과값(데이터)를 쓰겠다는 의미
+            c = a - b; Console.WriteLine(c);
+            c = a * b; Console.WriteLine(c);
+            c = a / b; Console.WriteLine(c); // int끼리 나눴을 때는 몫만 반환됨.
+            c = a % b; Console.WriteLine(c); // 나머지 연산
+
+            // 복합 대입 연산자
+            c += a; Console.WriteLine(c); // c = c + a;와 같은 의미
+            c -= a; Console.WriteLine(c);
+            c *= a; Console.WriteLine(c);
+            c /= a; Console.WriteLine(c);
+            c %= a; Console.WriteLine(c);
+
+            // 증감 연산자
+            ++c; Console.WriteLine(c); // 전위 연산자. c += 1;과 같은 의미임. 연산 내용은 c = c + 1이고, 반환값도 c + 1임.
+            --c; Console.WriteLine(c);
+            c++; Console.WriteLine(c); // 후위 연산자. c += 1;과 같은 의미임. 연산 내용은 c = c + 1이지만, 반환값은 c임.
+            c--; Console.WriteLine(c);
+
+            // 관계 연산자(비교 연산자)
+            // 두 피연산자의 관계를 비교해서 결과가 참인지 거짓인지(bool 타입) 반환
+            bool result;
+            result = a == b; Console.WriteLine(result); // a와 b가 같으면 true, 다르면 false
+            result = a != b; Console.WriteLine(result); // a와 b가 다르면 true, 같으면 false
+            result = a > b; Console.WriteLine(result);
+            result = a < b; Console.WriteLine(result);
+            result = a >= b; Console.WriteLine(result);
+            result = a <= b; Console.WriteLine(result);
+
+            // 논리 연산자
+            // 논리형의 피연산자에 대해서만 연산 수행
+            bool A = true;
+            bool B = false;
+            result = A | B; Console.WriteLine(result); // or: A와 B  중에 하나라도 true면 true, 아니면 false
+            result = A & B; Console.WriteLine(result); // and: A와 B 둘 다 true면 true, 아니면 false
+            result = A ^ B; Console.WriteLine(result); // xor: A와 B 중에 하나만 true면 true, 아니면 false.
+            result = !A; Console.WriteLine(result); // not: A가 true면 false, false면 true
+            result = A == false; // not과 동일, !가 잘 안 보이기 때문에 사용
+
+            // 조건부 논리 연산자
+            result = A || B; // Conditional or: A가 true일 경우 논리 연산을 수행하지 않고 true 반환, 아니면 A | B를 수행
+            result = A && B; // Conditional and: A가 false일 경우 논리 연산을 수행하지 않고 false 반환, 아니면 A & B를 수행
+
+            // 비트 연산자
+            // 정수형의 피연산자에 대해서만 수행
+            int d = 0;
+            d = a | b; Console.WriteLine(d); // or: 같은 비트에서 하나라도 1이면 1, 아니면 0
+            // a == 14 == 2^3 + 2^2 + 2^1 == ...00001110
+            // b == 6 == 2^2 + 2^1        == ...00000110
+            // d                          == ...00001110 == 14
+            d = a & b; Console.WriteLine(d); // and: 같은 비트에서 둘 다 1이면 1, 아니면 0
+            d = a ^ b; Console.WriteLine(d); // xor: 같은 비트에서 A와 B 중에 하나만 1이면 1, 아니면 false
+            d = ~a; Console.WriteLine(d); // not: 비트가 1이면 0, 0이면 1
+            d = a << 2; Console.WriteLine(d); // shift -left: 비트를 n자리만큼 왼쪽으로 밂.
+            // a = ...00001110
+            // d = ...00111000 = 56
+            d = a >> 2; Console.WriteLine(d); // shift -right: 비트를 n자리만큼 오른쪽으로 밂.
+        }
+
+        // 전위 연산자를 함수로 표현한 것.
+        int BeforePP(ref int variable) // ref 키워드: 파라미터를 참조 타입으로 쓰겠다는 키워드. 즉, 인자로 넣어줄 수 있는 것은 주소를 가지고 있는 변수 같은 메모리 영역이고 상수는 넣을 수 없음.
+        {
+            variable += 1;
+            return variable;
+        }
+
+        // 후위 연산자를 함수로 표현한 것.
+        int  AfterPP(ref int variable)
+        {
+            int origin = variable;
+            variable += 1;
+            return origin;
+        }
+    }
+}
