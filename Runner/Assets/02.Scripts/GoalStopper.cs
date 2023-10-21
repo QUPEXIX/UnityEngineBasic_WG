c99529d6b7e88d871d1873b04819b270a54ed1dc
QUPEXIX <143862560+QUPEXIX@users.noreply.github.com>
2023-10-21 18:43:49 +0900

    Unity - 애니메이션, 충돌
    1. 애니메이션
    2. 충돌

---
+++ Runner/Assets/02.Scripts/GoalStopper.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing.Text;
+using UnityEngine;
+
+public class GoalStopper : MonoBehaviour
+{
+    private int _grade;
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.TryGetComponent(out Runner runner))
+        {
+            runner.Finish(_grade++);
+        }
+    }
+}
--- Runner/Assets/02.Scripts/Runner.cs
+++ Runner/Assets/02.Scripts/Runner.cs
@@ -4,13 +4,90 @@ using UnityEngine;
 
 public class Runner : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3.0f;
+    public bool isMovable
+    {
+        get => _isMovable;
+        set
+        {
+            _isMovable = value;
+            if (value == false)
+            {
+                speedModified = 0.0f;
+            }
+        }
+    }
+    private bool _isMovable = true;
 
-    // ÀÌµ¿°Å¸® = ¼Ó·Â * ½Ã°£
-    // °íÁ¤ ÇÁ·¹ÀÓ´ç ÀÌµ¿°Å¸® = ¼Ó·Â * °íÁ¤ ÇÁ·¹ÀÓ°£ ½Ã°£º¯È­
+    public float speedModified
+    {
+        get => _speedModified;
+        set
+        {
+            _speedModified = value;
+            _animator.SetFloat("Speed", value);
+        }
+    }
+
+    [SerializeField] private float _speed = 3.0f;
+    private float _speedModified;
+
+    [SerializeField] private float _speedModifyingPeriod = 1.0f;
+    private float _modifyingTimer;
+    [Range(0.0f, 1.0f)][SerializeField] private float _stability;
+
+    private Animator _animator;
+
+    public void Finish(int grade)
+    {
+        isMovable = false;
+
+        switch (grade)
+        {
+            case 0:
+                _animator.Play("Jumping");
+                break;
+            case 1:
+            case 2:
+                _animator.Play("Salute");
+                break;
+            default:
+                _animator.Play("KneelDown");
+                break;
+        }
+
+        PlayManager.instance.RegisterRunnerFinished(this);
+    }
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        PlayManager.instance.RegisterRunner(this);
+    }
+        
+
+    // ì´ë™ê±°ë¦¬ = ì†ë ¥ * ì‹œê°„
+    // ê³ ì • í”„ë ˆì„ë‹¹ ì´ë™ê±°ë¦¬ = ì†ë ¥ * ê³ ì • í”„ë ˆì„ê°„ ì‹œê°„ë³€í™”
     private void FixedUpdate()
     {
+        if (_isMovable == false)
+        {
+            return;
+        }
+
+        if (_modifyingTimer <= 0.0f)
+        {
+            speedModified = _speed * Random.Range(_stability, 1.0f);
+            _modifyingTimer = _speedModifyingPeriod;
+        }
+        else
+        {
+            _modifyingTimer -= Time.fixedDeltaTime;
+        }
         //transform.position += Vector3.forward * _speed * Time.fixedDeltaTime;
-        transform.Translate(Vector3.forward * _speed * Time.fixedDeltaTime);
+        transform.Translate(Vector3.forward * _speedModified * Time.fixedDeltaTime);
     }
 }
