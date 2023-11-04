c99a8db667bae0a4db4bcc4551ff4e484c2cc308
QUPEXIX <143862560+QUPEXIX@users.noreply.github.com>
2023-11-04 18:55:34 +0900

    Unity - 2D 플랫포머 1
    2D 플랫포머 1

---
+++ 2D_Platformer/Platformer/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 moveInput;
+    private float walkSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+}
